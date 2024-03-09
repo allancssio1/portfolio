a937fe7a78d0e9084bec25491f8e8a25ab3094ff
Allan Cassio <allan.cassio1@gmail.com>
2024-03-09 06:12:08 -0300

    created footer

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="h-14 h-full flex items-center justify-center bg-gray-950">
+      <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400">
+        Feito por <strong className="font-medium">Allan Cássio</strong>
+      </span>
+    </footer>
+  )
+}
