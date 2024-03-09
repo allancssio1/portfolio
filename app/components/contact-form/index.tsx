40d0f35e518b4e5c9ffbc7694a010c4b6e55e8b0
Allan Cassio <allan.cassio1@gmail.com>
2024-03-09 07:40:15 -0300

    create form

--- app/components/contact-form/index.tsx
+++ app/components/contact-form/index.tsx
@@ -1,8 +1,27 @@
+'use client'
+
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Button } from '../button'
 import { SectionTitle } from '../section-title'
+import { useForm } from 'react-hook-form'
+import { TypeOf, z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const contactFormSchema = z.object({
+  name: z.string().min(3).max(100),
+  email: z.string().email(),
+  mensage: z.string().min(5).max(500),
+})
+
+type ContactFormData = z.infer<typeof contactFormSchema>
 
 export const ContactForm = () => {
+  const { handleSubmit, register } = useForm<ContactFormData>({
+    resolver: zodResolver(contactFormSchema),
+  })
+
+  const onSubmit = async ({ name, email, mensage }: ContactFormData) => {}
+
   return (
     <section className="py-16 px-6 md:py-32 flex items-center justify-center bg-gray-950">
       <div className="w-full max-w-[420px] mx-auto">
@@ -12,20 +31,26 @@ export const ContactForm = () => {
           className="items-center text-center"
         />
 
-        <form action="" className="mt-12 w-full flex flex-col gap-4">
+        <form
+          className="mt-12 w-full flex flex-col gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
             type="text"
-            placeholder="nome"
+            placeholder="Nome"
             className="w-full h-14 bg-gray-800 rounded-xl placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-1 ring-emerald-600"
+            {...register('name')}
           />
           <input
             type="email"
             placeholder="E-mail"
             className="w-full h-14 bg-gray-800 rounded-xl placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-1 ring-emerald-600"
+            {...register('email')}
           />
           <textarea
             placeholder="Mensagem"
             className="resize-none  w-full h-[138px] bg-gray-800 rounded-xl placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-1 ring-emerald-600"
+            {...register('mensage')}
           />
 
           <Button className="mt-6 w-max mx-auto shadow-button">
